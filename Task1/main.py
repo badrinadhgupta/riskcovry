4e09bc283af6983c6cda5fc5659eef348bee4801
Dhruv Shetty <dhruvshetty3@gmail.com>
2021-06-06 01:28:04 +0530

    Updating main.py to also take form response

--- Task1/main.py
+++ Task1/main.py
@@ -30,22 +30,53 @@ class ApiCalls(Resource):
         decode_string = base64.b64decode(baudio+b"==")
         ogg_file = open("temp.ogg", "wb")
         ogg_file.write(decode_string)
-        data, samplerate = sf.read('temp.ogg')
-        sf.write('temp.wav', data, samplerate)
-        r=sr.Recognizer()
-        read_file = open("temp.wav", "rb")
-        with sr.AudioFile(read_file) as source:
-            audio_data = r.record(source)
-            t=r.recognize_google(audio_data)
-            print(t)
-            if(question_key=="q1"):
-                return {"answers":q1(t,options)}
-            elif(question_key=="q2"):
-                return {"answers":q2(t,options)}
-            elif(question_key=="q3"):
-                return {"answers":q3(t,options)}
-            else:
-                return {"error":"Correct Question key was not provided"}
+        return callSpecificQuestion(ogg_file,question_key,options)
+
+    
+    
+    
+@app.route('/form', methods=['GET','POST'])
+def form_example():
+    # handle the POST request
+    if request.method == 'POST':
+        audio = request.files.get("audio")
+        print(audio)
+        question_key = request.form.get("question_key")
+        options = eval(request.form.get("options"))
+        answer = callSpecificQuestion(audio,question_key,options)
+        return '''
+                  <h1>The answer is: {}</h1>'''.format(answer)
+
+    # otherwise handle the GET request
+    return '''
+           <form method="POST" enctype="multipart/form-data">
+           <label for="fname">Question Key:</label>
+  <input type="text" id="question_key" name="question_key" value=""><br><br>
+  <label for="fname">Options:</label>
+  <input type="text" id="options" name="options" value=""><br><br>
+  <label for="img">Upload audio file:</label>
+  <input type="file" id="audio" name="audio" accept="audio/*"><br><br>
+  <input type="submit" value="Submit">
+</form> 
+           </form>'''
+
+def callSpecificQuestion(audio,question_key,options):
+    data, samplerate = sf.read(audio)
+    sf.write('temp.wav', data, samplerate)
+    r=sr.Recognizer()
+    read_file = open("temp.wav", "rb")
+    with sr.AudioFile(read_file) as source:
+        audio_data = r.record(source)
+        t=r.recognize_google(audio_data)
+        print(t)
+        if(question_key=="q1"):
+            return {"answers":q1(t,options)}
+        elif(question_key=="q2"):
+            return {"answers":q2(t,options)}
+        elif(question_key=="q3"):
+            return {"answers":q3(t,options)}
+        else:
+            return {"error":"Correct Question key was not provided"}
 
 api.add_resource(ApiCalls, '/')
 
@@ -53,8 +84,6 @@ api.add_resource(ApiCalls, '/')
 if __name__ == '__main__':
     app.run(debug=True)
 
-def helloWorld():
-  return "Hello, cross-origin-world!"
 
 app.run()
 
