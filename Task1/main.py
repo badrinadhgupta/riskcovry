483570f812086dccc2dca48cb60e5043a62858f1
Dhruv Shetty <dhruvshetty3@gmail.com>
2021-06-06 00:38:12 +0530

    Fixing issues in Main

--- Task1/main.py
+++ Task1/main.py
@@ -4,7 +4,9 @@ from flask_restful import Resource, Api
 import speech_recognition as sr
 import base64
 import soundfile as sf
-import q1,q2,q3
+from q1 import q1
+from q2 import q2
+from q3 import q3
 
 app = Flask(__name__)
 app.config["DEBUG"] = True
@@ -25,23 +27,25 @@ class ApiCalls(Resource):
         question_key = request.args.get("question_key")
         options = request.args.get("options")
         baudio = bytes(audio, 'utf-8')
-        decode_string = base64.b64decode(baudio+b"==", validate=True)
+        decode_string = base64.b64decode(baudio+b"==")
         ogg_file = open("temp.ogg", "wb")
         ogg_file.write(decode_string)
         data, samplerate = sf.read('temp.ogg')
         sf.write('temp.wav', data, samplerate)
         r=sr.Recognizer()
         read_file = open("temp.wav", "rb")
-        audio_data = r.record(sr.AudioFile(read_file))
-        t=r.recognize_google(audio_data)
-        if(question_key=="q1"):
-            return {"asnwers":q1(t,options)}
-        elif(question_key=="q2"):
-            return {"asnwers":q2(t,options)}
-        elif(question_key=="q3"):
-            return {"asnwers":q3(t,options)}
-        else:
-            return {"error":"Correct Question key was not provided"}
+        with sr.AudioFile(read_file) as source:
+            audio_data = r.record(source)
+            t=r.recognize_google(audio_data)
+            t=r.recognize_google(audio_data)
+            if(question_key=="q1"):
+                return {"asnwers":q1(t,options)}
+            elif(question_key=="q2"):
+                return {"asnwers":q2(t,options)}
+            elif(question_key=="q3"):
+                return {"asnwers":q3(t,options)}
+            else:
+                return {"error":"Correct Question key was not provided"}
 
 api.add_resource(ApiCalls, '/')
 
